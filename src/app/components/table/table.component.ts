d98c4ce20c0eabe8e452d0cb7c5685a5de0dbb69
Azat Farmanyan <azat.farmanyan2@mail.ru>
2025-07-31 15:26:47 +0400

    create table and pagination

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -1,11 +1,26 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
+import { PaginationComponent } from '../pagination/pagination.component';
 
 @Component({
   selector: 'app-table',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, PaginationComponent],
   templateUrl: './table.component.html',
   styleUrl: './table.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class TableComponent {}
+export class TableComponent {
+  readonly tableData = Array.from({ length: 15 }, () => ({
+    date: '10/09/24 18:16',
+    id: '1388223',
+    phone: '+ 7 777 777-77-77',
+    transactionId: '25843594321',
+    description: 'Пополнение баланса',
+    amount: '5.00 ₽',
+    user: 'Admin1',
+    method: 'Нал.',
+    account: 'Игровой',
+    link: 'Link',
+    extra: '--',
+  }));
+}
